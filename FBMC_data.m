a78d5c096cb85cb683810ed38c99b3338b5478af
Kaygo <hamchampion@163.com>
2017-11-08 19:31:51 +0800

    Update FBMC_data.m

--- FBMC_data.m
+++ FBMC_data.m
@@ -1,3 +1,4 @@
+%author:KAI
 function [BinaryDataStream_FBMC_Aux,x_FBMC_Aux,s_FBMC_Aux]= FBMC_data(AuxiliaryMethod,FBMC,PAM)
     BinaryDataStream_FBMC_Aux = randi([0 1],AuxiliaryMethod.NrTransmittedSymbols*log2(PAM.ModulationOrder),1);
     xD_FBMC_Aux = PAM.Bit2Symbol(BinaryDataStream_FBMC_Aux);
